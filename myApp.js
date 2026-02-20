b3625f7531c7b33ed3d959d4051a68947fd2ae48
Sav <syc9459@nyu.edu>
2026-02-20 15:49:20 -0500

    commit changes commit changes

---
+++ myApp.js
@@ -0,0 +1,13 @@
+var colors = ["#c81501", "#112613", "#c06471", "#142714", "#acb0b3"];
+var i = 0;
+
+function changeColor() {
+    document.body.style.backgroundColor = colors[i];
+    i = i + 1;
+
+    if (i == colors.length) {
+        i = 0;
+    }
+}
+
+setInterval(changeColor, 2000);
